993116ebc7b390f8b8812e4c4d52ac0f9ae2f942
Vuong Huynh <mannhuynh@gmail.com>
2022-01-24 16:01:09 -0600

    Add ex3 lvl2

---
+++ level_2/ex3/ex3.go
@@ -0,0 +1,17 @@
+package ex3
+
+import "fmt"
+
+const (
+	// UNTYPED constants
+	a = "Hello"
+ 	b = 31
+
+// TYPED constants
+ 	c string = "World!"
+	d int = 32
+)
+func Ex3(){
+	
+	fmt.Println(a,b,c,d)
+}
