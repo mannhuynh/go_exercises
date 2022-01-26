722dc094b11e77f402c2acda51ec39e137adae67
Vuong Huynh <mannhuynh@gmail.com>
2022-01-26 16:46:31 -0600

    Add Ninja lvl4 - ex4

---
+++ level_4/ex4/ex.go
@@ -0,0 +1,21 @@
+package ex4
+
+import "fmt"
+
+func Ex4() {
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+
+	x = append(x, 52)
+	fmt.Println(x)
+
+	x = append(x, 53, 54, 55)
+	fmt.Println(x)
+
+	y := []int{56, 57, 58, 59, 60}
+
+	// y... is spread or unpack slice y using ... operator
+	x = append(x, y...)
+
+	fmt.Println(x)
+	fmt.Println("---------------")
+}
