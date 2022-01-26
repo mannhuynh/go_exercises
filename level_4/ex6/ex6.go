afd8ed408852c3c47461db0ab14acae58a187a69
Vuong Huynh <mannhuynh@gmail.com>
2022-01-26 17:20:28 -0600

    Add Ninja lvl4 - ex6

---
+++ level_4/ex6/ex6.go
@@ -0,0 +1,26 @@
+package ex6
+
+import "fmt"
+
+func Ex6() {
+	x := make([]string, 50)
+
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+	fmt.Println(x)
+
+	// Using append will append 5 states into the first 50 zero values
+	x = append(x, ` Alabama`, ` Alaska`, ` Arizona`, ` Arkansas`, ` California`, ` Colorado`, ` Connecticut`, ` Delaware`, ` Florida`, ` Georgia`, ` Hawaii`, ` Idaho`, ` Illinois`, ` Indiana`, ` Iowa`, ` Kansas`, ` Kentucky`, ` Louisiana`, ` Maine`, ` Maryland`, ` Massachusetts`, ` Michigan`, ` Minnesota`, ` Mississippi`, ` Missouri`, ` Montana`, ` Nebraska`, ` Nevada`, ` New Hampshire`, ` New Jersey`, ` New Mexico`, ` New York`, ` North Carolina`, ` North Dakota`, ` Ohio`, ` Oklahoma`, ` Oregon`, ` Pennsylvania`, ` Rhode Island`, ` South Carolina`, ` South Dakota`, ` Tennessee`, ` Texas`, ` Utah`, ` Vermont`, ` Virginia`, ` Washington`, ` West Virginia`, ` Wisconsin`, ` Wyoming`)
+
+	for i := 0; i < len(x); i++ {
+		fmt.Print(x[i])
+
+	}
+
+	// the length and the capacity have been changed
+	fmt.Println()
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+}
--- level_4/main.go
+++ level_4/main.go
@@ -6,6 +6,7 @@ import (
 	"myapp/ex3"
 	"myapp/ex4"
 	"myapp/ex5"
+	"myapp/ex6"
 )
 
 func main() {
@@ -14,4 +15,5 @@ func main() {
 	ex3.Ex3()
 	ex4.Ex4()
 	ex5.Ex5()
+	ex6.Ex6()
 }
