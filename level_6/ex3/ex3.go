7a0a2ad7e03c3a3c7d77f6d1dfbafe0b046e2ebd
Vuong Huynh <vhuynh39@gatech.edu>
2022-02-08 16:43:44 -0600

    Add level 6 - ex3

--- level_6/ex3/ex3.go
+++ level_6/ex3/ex3.go
@@ -1,1 +1,21 @@
 package ex3
+
+import "fmt"
+
+func Ex3() {
+	defer foo()
+	bar()
+	wutang()
+}
+
+func foo() {
+	fmt.Println("This is foo and will go after all because I am deferred")
+}
+
+func bar() {
+	fmt.Println("I am bar and I will go before foo")
+}
+
+func wutang() {
+	fmt.Println("I will go wherever I am placed!!! But before foo ")
+}
