07264d3925811bd8b6581a890736486b6902c54d
Vuong Huynh <mannhuynh@gmail.com>
2022-02-15 15:33:28 -0600

    Add level 8 - ex2

---
+++ level_8/ex2/ex2.go
@@ -0,0 +1,36 @@
+package ex2
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+var s = `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
+
+// Create a struct type of person followed the JSON structure
+type person struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
+
+// Using json.Marshal to convert a JSON to a struct
+func Ex2() {
+
+	fmt.Println(s)
+
+	// First, We need to convert the JSON to a byte slice before using Unmarshal
+	bs := []byte(s)
+
+	// Next, we need a slice of type person
+	people := []person{}
+
+	// Use Unmarshal to add the byte slice of s to people
+	// Specially, we need to reference to people pointer.
+	err := json.Unmarshal(bs, &people)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(people)
+}
