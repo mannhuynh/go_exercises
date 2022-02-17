d3b741b0f8bd67e3b14c21adeab47dc0ba4eec55
Vuong Huynh <mannhuynh@gmail.com>
2022-02-17 11:55:39 -0600

    Add Ninja lvl 9 - ex4

--- level_9/ex4/ex4.go
+++ level_9/ex4/ex4.go
@@ -1,36 +1,37 @@
-package ex3
+package ex4
 
 import (
 	"fmt"
-	"runtime"
 	"sync"
 )
 
-func Ex3() {
+func Ex4() {
 	var counter int
 	var wg sync.WaitGroup
+	var mux sync.Mutex
 
 	wg.Add(2)
 
 	go func() {
-		for i := 0; i < 3; i++ {
+		for i := 0; i < 5; i++ {
+			mux.Lock()
 			v1 := counter
-			runtime.Gosched()
 			v1++
 			counter = v1
 			fmt.Println("Counter for v1:", counter)
+			mux.Unlock()
 		}
 		wg.Done()
 	}()
 
 	go func() {
-		for j := 0; j < 5; j++ {
+		for j := 0; j < 10; j++ {
+			mux.Lock()
 			v2 := counter
-			runtime.Gosched()
-
 			v2++
 			counter = v2
 			fmt.Println("Counter for v2:", counter)
+			mux.Unlock()
 		}
 		wg.Done()
 	}()
